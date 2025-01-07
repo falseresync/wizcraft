bc91c95fa91294eab516fe06d811e40d30718188
falseresync <falseresync@gmail.com>
2025-01-07 23:17:54 +0300

    Animate Focus Picker

--- src/main/java/falseresync/wizcraft/client/hud/FocusPickerHudItem.java
+++ src/main/java/falseresync/wizcraft/client/hud/FocusPickerHudItem.java
@@ -2,13 +2,14 @@ package falseresync.wizcraft.client.hud;
 
 import com.mojang.blaze3d.systems.RenderSystem;
 import falseresync.lib.client.BetterDrawContext;
+import falseresync.lib.math.Easing;
 import falseresync.wizcraft.client.WizcraftClient;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.render.RenderTickCounter;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.Identifier;
-import org.joml.Matrix4fStack;
+import org.joml.Matrix4f;
 
 import java.util.LinkedList;
 
@@ -22,11 +23,19 @@ public class FocusPickerHudItem implements HudItem {
     private static final int ITEM_W = 16;
     private static final int ITEM_H = 16;
     private static final int TEXT_H = 8;
+    private static final int DISPLAY_DURATION = 60;
+    private static final int PARENT_ANIMATION_DURATION = 8;
+    private static final int ITEMS_ANIMATION_DURATION = 6;
     private final MinecraftClient client;
     private final TextRenderer textRenderer;
     private LinkedList<ItemStack> focuses = new LinkedList<>();
+    private float baseOpacity = 1;
+    private boolean isVisible = false;
     private int remainingDisplayTicks = 0;
-    private float opacity = 1;
+    private boolean animatingParent = false;
+    private int remainingParentAnimationTicks = 0;
+    private boolean animatingItems = false;
+    private int remainingItemsAnimationTicks = 0;
 
     public FocusPickerHudItem(MinecraftClient client, TextRenderer textRenderer) {
         this.client = client;
@@ -35,8 +44,8 @@ public class FocusPickerHudItem implements HudItem {
 
     @Override
     public void render(BetterDrawContext context, RenderTickCounter tickCounter) {
-        if (isVisible()) {
-            opacity = Math.min(1, remainingDisplayTicks / 10f);
+        if (isVisible() || animatingParent) {
+            baseOpacity = getAnimatedBaseOpacity();
             var yOffsetPerItem = ITEM_H + MARGIN;
             var yOffset = (Math.min(focuses.size(), 3) - 1) * yOffsetPerItem;
             var widgetW = SEL_TEX_W;
@@ -47,7 +56,7 @@ public class FocusPickerHudItem implements HudItem {
             var y = context.getScaledWindowHeight() / 2 - widgetH / 2;
 
             RenderSystem.enableBlend();
-            context.setShaderColor(1, 1, 1, opacity);
+            context.setShaderColor(1, 1, 1, baseOpacity);
 
             var selTexX = x;
             var selTexY = y + yOffset;
@@ -55,46 +64,87 @@ public class FocusPickerHudItem implements HudItem {
 
             var itemX = x + widgetW / 2 - ITEM_W / 2;
 
-            var selected = focuses.peekFirst();
-            var item1Y = y + SEL_TEX_H / 2 - ITEM_H / 2 + yOffset;
-            context.drawItemWithoutEntity(selected, itemX, item1Y);
+            if (animatingItems) {
+                var item1 = focuses.peekLast();
+                int item1Y = y + SEL_TEX_H / 2 - ITEM_H / 2 + yOffset;
+                float item1Scale = (float) Easing.easeOutCirc((double) (remainingItemsAnimationTicks) / ITEMS_ANIMATION_DURATION);
+                float item1Translation = (float) (SEL_TEX_H * Easing.easeInSine((double) (ITEMS_ANIMATION_DURATION - remainingItemsAnimationTicks) / ITEMS_ANIMATION_DURATION));
+                float item1Opacity = baseOpacity * remainingItemsAnimationTicks / ITEMS_ANIMATION_DURATION;
+                paintItem(context, item1, itemX, item1Y, item1Scale, item1Translation, item1Opacity, false);
 
-            if (focuses.size() > 1) {
-                var next = focuses.get(1);
-                var item2Y = y + Math.min(focuses.size() - 2, 1) * yOffsetPerItem + yOffsetPerItem / 2 - ITEM_H / 2;
-                paintScaledTinted(context, next, itemX, item2Y, 0.85f, false);
-            }
+                if (focuses.size() > 1) {
+                    var item2 = focuses.peekFirst();
+                    int item2Y = y + Math.min(focuses.size() - 2, 1) * yOffsetPerItem + yOffsetPerItem / 2 - ITEM_H / 2;
+                    float item2Scale = 0.85f + (1 - 0.85f) * (float) Easing.easeOutSine((double) (ITEMS_ANIMATION_DURATION - remainingItemsAnimationTicks) / ITEMS_ANIMATION_DURATION);
+                    float item2Translation = (float) (yOffsetPerItem * Easing.easeInOutSine((double) (ITEMS_ANIMATION_DURATION - remainingItemsAnimationTicks) / ITEMS_ANIMATION_DURATION));
+                    paintItem(context, item2, itemX, item2Y, item2Scale, item2Translation, baseOpacity, false);
+                }
 
-            if (focuses.size() > 2) {
-                var next2 = focuses.get(2);
-                var item3Y = y + yOffsetPerItem / 2 - ITEM_H / 2;
-                paintScaledTinted(context, next2, itemX, item3Y, 0.70f, true);
+                if (focuses.size() > 2) {
+                    var item3 = focuses.get(1);
+                    int item3Y = y + yOffsetPerItem / 2 - ITEM_H / 2;
+                    float item3Scale = 0.70f * (float) Easing.easeOutSine((double) (ITEMS_ANIMATION_DURATION - remainingItemsAnimationTicks) / ITEMS_ANIMATION_DURATION);
+                    float item3Translation = (float) (yOffsetPerItem * Easing.easeInOutSine((double) (ITEMS_ANIMATION_DURATION - remainingItemsAnimationTicks) / ITEMS_ANIMATION_DURATION));
+                    paintItem(context, item3, itemX, item3Y, item3Scale, item3Translation, baseOpacity, false);
+
+                    var item4 = focuses.get(2);
+                    int item4Y = y + yOffsetPerItem / 2 - ITEM_H / 2;
+                    float item4Scale = 0.70f * (float) Easing.easeInOutQuad((double) (ITEMS_ANIMATION_DURATION - remainingItemsAnimationTicks) / ITEMS_ANIMATION_DURATION);
+                    paintItem(context, item4, itemX, item4Y, item4Scale, 0f, baseOpacity / 2, true);
+                }
+            } else {
+                var item1 = focuses.peekFirst();
+                var item1Y = y + SEL_TEX_H / 2 - ITEM_H / 2 + yOffset;
+                context.drawItemWithoutEntity(item1, itemX, item1Y);
+                paintItem(context, item1, itemX, item1Y, 1f, 0f, baseOpacity,false);
+
+                if (focuses.size() > 1) {
+                    var item2 = focuses.get(1);
+                    var item2Y = y + Math.min(focuses.size() - 2, 1) * yOffsetPerItem + yOffsetPerItem / 2 - ITEM_H / 2;
+                    paintItem(context, item2, itemX, item2Y, 0.85f, 0f, baseOpacity,false);
+                }
+
+                if (focuses.size() > 2) {
+                    var item3 = focuses.get(2);
+                    var item3Y = y + yOffsetPerItem / 2 - ITEM_H / 2;
+                    paintItem(context, item3, itemX, item3Y, 0.70f, 0f, baseOpacity, true);
+                }
             }
 
             RenderSystem.disableBlend();
         }
     }
 
-    protected void paintScaledTinted(BetterDrawContext context, ItemStack stack, int x, int y, float scale, boolean shouldTint) {
-        Matrix4fStack view = RenderSystem.getModelViewStack();
-        view.pushMatrix();
-        view.scaleAround(scale, scale, 1f, x + ITEM_W / 2f, y + ITEM_H / 2f, 0);
-        RenderSystem.applyModelViewMatrix();
-
-        if (shouldTint) context.setShaderColor(161 / 255f, 158 / 255f, 170 / 255f, opacity / 2);
+    protected void paintItem(BetterDrawContext context, ItemStack stack, int x, int y, float scale, float translation, float opacity, boolean shouldTint) {
+        var matrices = context.getMatrices();
+        matrices.push();
+        matrices.multiplyPositionMatrix(new Matrix4f().scaleAround(scale, scale, 1f, x + ITEM_W / 2f, y + ITEM_H / 2f, 0));
+        matrices.translate(0, translation, 0);
+        if (shouldTint) {
+            context.setShaderColor(161 / 255f, 158 / 255f, 170 / 255f, opacity);
+        } else {
+            context.setShaderColor(1, 1, 1, opacity);
+        }
 
         context.drawItemWithoutEntity(stack, x, y);
 
-        if (shouldTint) context.setShaderColor(1, 1, 1, opacity);
+        context.setShaderColor(1, 1, 1, baseOpacity);
+        matrices.pop();
+    }
 
-        view.popMatrix();
-        RenderSystem.applyModelViewMatrix();
+    private float getAnimatedBaseOpacity() {
+        if (animatingParent) {
+            return isVisible()
+                    ? 1 - (float) remainingParentAnimationTicks / PARENT_ANIMATION_DURATION
+                    : (float) remainingParentAnimationTicks / PARENT_ANIMATION_DURATION;
+        }
+        return 1;
     }
 
     @Override
     public void tick() {
         if (client.player == null) {
-            hide();
+            clear();
             return;
         }
 
@@ -103,18 +153,58 @@ public class FocusPickerHudItem implements HudItem {
 
             if (remainingDisplayTicks == 0) {
                 hide();
-                return;
+            }
+        }
+
+        if (remainingParentAnimationTicks > 0) {
+            remainingParentAnimationTicks -= 1;
+
+            if (remainingParentAnimationTicks == 0) {
+                animatingParent = false;
+
+                if (remainingDisplayTicks == 0) {
+                    clear();
+                }
+            }
+        }
+
+        if (remainingItemsAnimationTicks > 0) {
+            remainingItemsAnimationTicks -= 1;
+
+            if (remainingItemsAnimationTicks == 0) {
+                animatingItems = false;
             }
         }
     }
 
     public void show() {
-        remainingDisplayTicks = 80;
+        if (!isVisible()) {
+            animateParent();
+        }
+        isVisible = true;
+        remainingDisplayTicks = DISPLAY_DURATION;
     }
 
     public void hide() {
-        remainingDisplayTicks = 0;
+        if (isVisible()) {
+            animateParent();
+        }
+        isVisible = false;
+    }
+
+    private void clear() {
+        isVisible = false;
         focuses.clear();
+    }
+
+    private void animateParent() {
+        animatingParent = true;
+        remainingParentAnimationTicks = PARENT_ANIMATION_DURATION;
+    }
+
+    private void animateItems() {
+        animatingItems = true;
+        remainingItemsAnimationTicks = ITEMS_ANIMATION_DURATION;
     }
 
     public void upload(LinkedList<ItemStack> newFocuses) {
@@ -134,7 +224,10 @@ public class FocusPickerHudItem implements HudItem {
     }
 
     public void pickNext() {
-        focuses.addLast(focuses.removeFirst());
+        if (focuses.size() > 1) {
+            focuses.addLast(focuses.removeFirst());
+            animateItems();
+        }
     }
 
     public ItemStack getCurrentlyPicked() {
@@ -142,6 +235,6 @@ public class FocusPickerHudItem implements HudItem {
     }
 
     public boolean isVisible() {
-        return remainingDisplayTicks > 0 && !focuses.isEmpty();
+        return isVisible && !focuses.isEmpty();
     }
 }
